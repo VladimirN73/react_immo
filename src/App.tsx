3ab12157b8bb39fe875fd14c2384fab31ca72c60
Vladimir Nalivaika <vladimir.nalivaika@bridging-it.de>
2022-01-15 18:40:36 +0100

    test dev env

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import * as components from './components/components';
 function App() {
   return (
     <div className="App">
-      v 0.0.4 is running
-      <components.ScreenCommon/>
+      v 0.0.5 is running
+      <components.ScreenCommon />
     </div>
   );
 }
