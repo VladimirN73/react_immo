630cba95661ab420774c3128bd9d9ab54f73c6be
Vladimir Nalivaika <vladimir.nalivaika@bridging-it.de>
2022-01-23 23:30:53 +0100

    continue

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,6 @@ import BodyElement from "./components/Body";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  React.useEffect(() => {
-    document.addEventListener("keydown", function (event: any) {
-      if (event.keyCode === 13 && event.target.nodeName === "INPUT") {
-        var form = event.target.form;
-        var index = Array.prototype.indexOf.call(form, event.target);
-        form.elements[index + 2].focus();
-        event.preventDefault();
-      }
-    });
-  }, []);
-
   return (
     <div className="App">
       <Header />
